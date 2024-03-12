e71930576b07f1c51581fe056af0f20033d1f456
manbarmohamed <you@example.com>
2024-03-12 15:34:23 +0000

    organization de code

---
+++ script.js
@@ -0,0 +1,42 @@
+let Questions=[
+    {
+        quetstion:"what's capital of maroc",
+        answer:[
+            "Fes",
+            "CasaBlanca",
+            "Algirie",
+            "Rabat",
+        ],
+        corect:"Rabat"
+    },
+    {
+        quetstion:"what's capital of Spain",
+        answer:[
+            "Barcalone",
+            "Bilbao",
+            "Madrid",
+            "Paris",
+        ],
+        corect:"Madrid"
+    },
+    {
+        quetstion:"what's capital of France",
+        answer:[
+            "Toulouse",
+            "Nes",
+            "Paris",
+            "Milan",
+        ],
+        corect:"Paris"
+    },
+    {
+        quetstion:"what's capital of Italie",
+        answer:[
+            "Napoli",
+            "Torino",
+            "Milan",
+            "Roma",
+        ],
+        corect:"Roma"
+    },
+]
