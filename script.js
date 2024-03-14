d6710f8baf7aead9fd9eb5650019def329e66a18
manbarmohamed <you@example.com>
2024-03-14 12:27:00 +0000

    AJOUTER LA SELECTION DE LA REPONSE CORRECT

--- script.js
+++ script.js
@@ -1,55 +1,110 @@
-let Questions=[
+let Questions = [
     {
-        quetstion:"what's capital of maroc",
-        answer:[
+        quetstion: "what's capital of maroc",
+        answer: [
             "Fes",
             "CasaBlanca",
             "Algerie",
             "Rabat",
         ],
-        corect:"Rabat"
+        corect: "Rabat"
     },
     {
-        quetstion:"what's capital of Spain",
-        answer:[
+        quetstion: "what's capital of Spain",
+        answer: [
             "Barcalone",
             "Bilbao",
             "Madrid",
             "Paris",
         ],
-        corect:"Madrid"
+        corect: "Madrid"
     },
     {
-        quetstion:"what's capital of France",
-        answer:[
+        quetstion: "what's capital of France",
+        answer: [
             "Toulouse",
             "Nes",
             "Paris",
             "Milan",
         ],
-        corect:"Paris"
+        corect: "Paris"
     },
     {
-        quetstion:"what's capital of Italie",
-        answer:[
+        quetstion: "what's capital of Italie",
+        answer: [
             "Napoli",
             "Torino",
             "Milan",
             "Roma",
         ],
-        corect:"Roma"
+        corect: "Roma"
     },
 ]
-let j=0
-let next_btn= document.getElementById('next-btn')
-function show(){
-    for(let i=0 ; i<Questions[j].answer.length; i++){
-        let qst= document.getElementById('Question');
 
-        let btn= document.getElementsByClassName('answer');
-        qst.innerHTML=Questions[j].quetstion;
-        btn[i].innerHTML=Questions[j].answer[i]
-    } 
-    j++   
+let score=0
+
+let nextscore=document.getElementById("next-score")
+
+let qst=document.getElementById("Question")
+let answerbuttons=document.getElementById("answer-buttons")
+let btns=document.getElementById("btns")
+let count=document.getElementById("count")
+
+let i=0
+
+function getData(){
+    for(let j=0;j<Questions[i].answer.length;j++){
+        qst.innerHTML=Questions[i].quetstion
+      answerbuttons.innerHTML +=
+        `<button class="answer" id="b1">${Questions[i].answer[j]}</button>`
+    }
+    count.innerHTML= `<p>${i + 1}  of ${Questions.length} Questions </p>`;
+
 }
-next_btn.onclick=show;
+getData();
+
+function Change(){
+    document.querySelectorAll(".answer").forEach((e)=>{
+        e.addEventListener("click",function(){
+            answerbuttons.innerHTML=""
+            qst.innerHTML=""
+            if(this.innerHTML == Questions[i].corect){
+                score +=10;
+                nextscore.innerHTML=
+                `<p>Your Score is ${score} </p>`;
+            }
+            
+            i++;
+            getData();
+            Change();
+           
+    
+    
+        })
+
+    }
+   )
+}
+Change();
+
+
+
+
+
+// let j = 0
+// let next_btn = document.getElementById('next-btn')
+// let qst = document.getElementById('Question');
+// let btn = document.getElementsByClassName('answer');
+
+// function show() {
+//     for (let i = 0; i < Questions[j].answer.length; i++) {
+
+//         qst.innerHTML = Questions[j].quetstion;
+//         btn[i].innerHTML = Questions[j].answer[i]
+//     }
+//     j++
+// }
+// next_btn.onclick = show;
+
+
+
